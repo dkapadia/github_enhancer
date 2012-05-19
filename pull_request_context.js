c8311fbc5595e831404718452a8d0dc74ea6939f
Dhruv Kapadia <git@dkapadia.com>
2012-05-19 16:55:48 -0400

    Somewhat working version without merging

--- pull_request_context.js
+++ pull_request_context.js
@@ -19,25 +19,19 @@ function insert_context(context_file_data, add_context_link){
     // Find which lines we want to add
     var hunk_info = get_hunk_info(add_context_link);
 
-
     // Find where we want to insert the lines of code
-    var first_hunk_row = add_context_link.closest('tr');
+    var first_hunk_row = add_context_link.closest('tr').next();
     var next_hunk_row = first_hunk_row.nextAll('tr.hunk-row').first();
     // TODO - this next hunk may not exist
 
     lines.each(function(index){
         var line_number = index + 1; // lines are 1 indexed
-            
-        // would be better to do this with ejs... or some templating code
-        var linenumbers = '<td class="line_numbers"></td><td class="line_numbers">' + line_number + '</td>';
-        // we need an extra space after td to account for the +/-
-        //  which are present in the commit diff, but not in the blob output
-        var line = $('<tr> ' + linenumbers + '<td><span>&nbsp;</span>' + $(this).html() + '</td></tr>');
-        
+
+        var line = _get_line_html(line_number, this)
         if(line_number < hunk_info['to_file_start_line'] && line_number > hunk_info['to_file_start_line'] - 5){
             line.insertBefore(first_hunk_row);
         }
-        
+
         var last_hunk_line = hunk_info['to_file_start_line'] + hunk_info['to_file_hunk_length'];
         if(line_number > last_hunk_line && line_number < last_hunk_line + 5){
             line.insertBefore(next_hunk_row);
@@ -64,11 +58,20 @@ function get_hunk_info(add_context_link){
     return hunk_info;
 }
 
+function _get_line_html(line_number, line_content){
+    // would be better to do this with ejs... or some templating code
+    var linenumbers = '<td class="line_numbers"></td><td class="line_numbers">' + line_number + '</td>';
+    // we need an extra space after td to account for the +/-
+    //  which are present in the commit diff, but not in the blob output
+    var line = $('<tr> ' + linenumbers + '<td><span>&nbsp;</span>' + $(line_content).html() + '</td></tr>');
+    return line;
+}
+
 $(document).ready(function(){
     var hunks = find_hunks();
     
     hunks.each( function(index) {
-        var add_context_link = $('<a href="#">Dhruv is the best</a>');
+        var add_context_link = $('<a href="#">Add Context</a>');
         $(this).append(add_context_link);
         add_context_link.click(get_context);
         // Add a class to the row that contains this hunk for easy searching
