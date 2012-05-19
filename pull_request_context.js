203a4ffa1f41c2abd064bf8c49bc97b5c1addd21
Dhruv Kapadia <git@dkapadia.com>
2012-05-19 12:53:14 -0400

    Add five lines of context before a hunk

--- pull_request_context.js
+++ pull_request_context.js
@@ -16,16 +16,24 @@ function insert_context(context_file_data, add_context_link){
     context_file_data = $(context_file_data);
     var lines = context_file_data.find('.line')
 
+    // Find which lines we want to add
+    var hunk_info = get_hunk_info(add_context_link);
+
+
     // Find where we want to insert the lines of code
     var parent_file = add_context_link.closest('.file');
     var data_table_body = parent_file.find('.data table tbody');
-    var first_data_table_row = data_table_body.find('tr').first()
+    var first_data_table_row = add_context_link.closest('tr')
 
     lines.each(function(index){
-        // would be better to do this with ejs... or some templating code
-        var linenumbers = '<td class="line_numbers"></td><td class="line_numbers">' + index + '</td>';
-        var line = $('<tr> ' + linenumbers + '<td>' + $(this).html() + '</td></tr>');
-        line.insertBefore(first_data_table_row);
+        var line_number = index + 1; // lines are 1 indexed
+        
+        if(line_number < hunk_info['to_file_start_line'] && line_number > hunk_info['to_file_start_line'] - 5){
+            // would be better to do this with ejs... or some templating code
+            var linenumbers = '<td class="line_numbers"></td><td class="line_numbers">' + line_number + '</td>';
+            var line = $('<tr> ' + linenumbers + '<td>' + $(this).html() + '</td></tr>');
+            line.insertBefore(first_data_table_row);
+        }
 
     })
 }
@@ -34,6 +42,18 @@ function find_hunks(){
     // this seems like something happening because of css minification? maybe I can't trust the class name?
     var hunks= $('.gc');
     return hunks;
+}
+
+function get_hunk_info(add_context_link){
+    var hunk_info_text = add_context_link.siblings('pre').text();
+    var hunk_info = {};
+    var hunk_regex = /@@.[-+]([0-9]+),([0-9]+).[-+]([0-9]+),([0-9]+).@@/;
+
+    var match = hunk_regex.exec(hunk_info_text);
+        
+    hunk_info['to_file_start_line'] = match[3];
+    hunk_info['to_file_hunk_length'] = match[4];
+    return hunk_info;
 }
 
 $(document).ready(function(){
