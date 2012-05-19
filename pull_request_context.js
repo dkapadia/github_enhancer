92e7f6e9f9995170f8151dc3dac5dc362a46357e
Dhruv Kapadia <git@dkapadia.com>
2012-05-19 00:02:55 -0400

    Add a dummy link after the 'view file at'

--- pull_request_context.js
+++ pull_request_context.js
@@ -1,3 +1,12 @@
-alert("blah");
-$('#diff-0').hide();
-alert("bluergh");
+
+$(document).ready(function(){
+    var files = $('.file')
+    
+    files.each( function(index) {
+        var view_file_link = $(this).find(".actions a");
+        view_file_link.after('<a>Dhruv is the best</a>');
+
+    });
+    $('#diff-0').hide();
+    alert("bluergh");
+});
